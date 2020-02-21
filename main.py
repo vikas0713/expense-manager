e8cad1b269a6e8fe3fe8f94514b7eac0a6420c00
vikas <vikas0713@gmail.com>
2020-02-21 12:07:16 +0530

    expense manager flow 1

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-from sql import UserTable, TripTable
+from sql import UserTable, TripTable, UserBalanceTable
 
 
 def add_trip():
     trip_name = input("Enter Trip Name : ")
     trip_obj = TripTable(name=trip_name)
-    trip_obj.insert()
+    return trip_obj.insert()
 
 def add_user():
     name = input("Enter full name: ")
     email = input("Enter Email :")
     ph_no = input("Enter phone number: ")
     user_obj = UserTable(name=name, email=email, ph_no=ph_no)
-    user_obj.insert()
+    return user_obj.insert()
+
+def associate_user_with_trip(trip_id, user_id, balance=0):
+    ub_table = UserBalanceTable(user_id=user_id, trip_id=trip_id, balance=0)
+    ub_table.insert()
+    print("User balance table updated")
+    
+
+def get_all_users():
+    user_obj = UserTable()
+    return user_obj.get_all_records()
     
 def expense_manager():
     print(" 1. Add Trip \n 2. Add User\n")
     option_selected = int(input("Enter option :"))
     print(option_selected) 
     if option_selected == 1:
-        add_trip()
+        trip_id= add_trip()
+        print("1. Add existing users \n 2. Add new User")
+        sub_option = int(input("Your option:  "))
+        if sub_option == 1:
+            all_existing_users = get_all_users()
+            for user in all_existing_users:
+                print("id: {},   name: {}".format(user["id"], user["name"]))
+            selected_users = input("Enter User id's")
+            selected_users = [int(x) for x in selected_users.split(",")]
+            print("Updating records.................")
+            for each_user in selected_users:
+                associate_user_with_trip(each_user, trip_id)
+            expense_manager()
+
+
+        elif sub_option == 2:
+            numer_of_users = input("Enter number of users: ")
+            for i in range(number_of_users):
+                add_trip()
+                # Associate to the trip
     elif option_selected == 2:
         add_user()
     else:
--- sql.py
+++ sql.py
@@ -3,17 +3,17 @@ from constants   import *
 
 
 class Connection(object):
-        """
-        Connection to MySQL Database, and gives you access to the cursor object
-        """
-        def __init__(self):
-            self.con = con.connect(host=HOST, database=DB_NAME, user=DB_USER, passwd=DB_PASS)
+    """
+    Connection to MySQL Database, and gives you access to the cursor object
+    """
+    def __init__(self):
+        self.con = con.connect(host=HOST, database=DB_NAME, user=DB_USER, passwd=DB_PASS)
 
-        def get_cursor(self):
-            return self.con.cursor()
+    def get_cursor(self):
+        return self.con.cursor()
 
-        def commit(self):
-            self.con.commit()
+    def commit(self):
+        self.con.commit()
 
 
 class UserTable(object):
@@ -32,14 +32,25 @@ class UserTable(object):
         raw_sql= sql_query.format(USER_TABLE, "Name","Email","Phone", self.name, self.email, self.ph_no)
         self.cursor.execute(raw_sql, (self.name, self.email, self.ph_no))
         self.con.commit()
+        return  self.cursor.lastrowid
 
     def get_user(self):
         sql_query = "SELECT * FROM  {} WHERE userId={}"
         raw_query = sql_query.format(USER_TABLE, self.id)
         print(raw_query)
         self.cursor.execute(raw_query)
-        
+
         return self.cursor.fetchone()
+
+    def get_all_records(self):
+        sql_query = "SELECT * FROM {}"
+        raw_query = sql_query.format(USER_TABLE)
+        self.cursor.execute(raw_query)
+        all_data = []
+        for record in self.cursor.fetchall():
+            all_data.append({"id":record[0], "name":record[1]})
+        return all_data
+
 
 
 class TripTable(object):
@@ -56,12 +67,22 @@ class TripTable(object):
         raw_query = sql_query.format(TRIP_TABLE, 'tripName')
         self.cursor.execute(raw_query, (self.trip_name,))
         self.con.commit()
+        return self.cursor.lastrowid
 
     def get_trip(self):
         sql_query = "SELECT * FROM {} WHERE tripId={}"
         raw_query = sql_query.format(TRIP_TABLE, self.trip_id)
         self.cursor.execute(raw_query)
         return self.cursor.fetchone()
+
+    def get_all_records(self):
+        sql_query = "SELECT * FROM {}"
+        raw_query = sql_query.format(TRIP_TABLE)
+        self.cursor.execute(raw_query)
+        all_data = []
+        for record in self.cursor.fetchall():
+            all_data.append({"id": record[0], "name":record[1]})
+        return all_data
 
 
 class UserBalanceTable(object):
